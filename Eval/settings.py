32a22b2a14e67b5f7f4938686ea7095f6e9fca48
alive <alive@agape.aizairenjian.com>
2015-04-22 23:33:06 +0800

    sa

--- Eval/settings.py
+++ Eval/settings.py
@@ -63,9 +63,15 @@ WSGI_APPLICATION = 'Eval.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'eval',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': 'onbsd12',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
     }
+
 }
 
 # Internationalization
